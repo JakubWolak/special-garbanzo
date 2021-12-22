a34336eee1103e401812e63f92f4bdd554cc38e7
Jakub <jwolak@cryptovoucher.io>
2021-12-22 22:11:08 +0100

    animation

--- components/common/Header.vue
+++ components/common/Header.vue
@@ -3,17 +3,17 @@
     <div class="header__content">
       <figure class="header__logo">
         <img
+          v-if="false"
           src="./../../static/logo.svg"
           alt="Jakub Wolak JavaScript Developer"
           class="header__logo__img"
         />
       </figure>
 
-      <nav>
-        <div>About me</div>
-        <div>Projects</div>
-        <CommonSmallPrimaryButton>Contact form</CommonSmallPrimaryButton>
-        <div></div>
+      <nav class="header__navigation">
+        <div class="header__navigation__button">About me</div>
+        <div class="header__navigation__button">Projects</div>
+        <CommonSmallPrimaryButton>Contact</CommonSmallPrimaryButton>
       </nav>
     </div>
   </header>
